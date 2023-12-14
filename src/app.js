1edcf97ba011b0d4ef1b86f706b557c215338565
PachyuChepe <pachyuchepe@gmail.com>
2023-12-14 21:34:16 +0900

    Feat: 회원가입 구현
    - 회원가입: 클라이언트 <-> 서버 통신 구현 성공
    - Redis: 노드메일러 인증번호 만료기한 10초 -> 180초로 변경
    - user.controller.js: 회원가입 성공 메세지 추가

--- src/app.js
+++ src/app.js
@@ -25,15 +25,19 @@ morganConfig(app);
 // CORS 설정
 app.use(
   // 아영님 cors 설정(localhost 원활한 동작을 위함)
-  cors({
-    credentials: true,
-    origin: ['http://localhost:5500']
-  })
-  // 윤호님 cors 설정
   // cors({
-  //   origin: [`https://localhost:${env.SERVER_PORT}`, 'https://www.vitahub.xyz'],
   //   credentials: true,
+  //   origin: ['http://localhost:5500'],
   // }),
+  // 윤호님 cors 설정
+  cors({
+    origin: [
+      `https://localhost:${env.SERVER_PORT}`,
+      `http://localhost:${env.SERVER_PORT}`,
+      'https://www.vitahub.xyz',
+    ],
+    credentials: true,
+  }),
 );
 
 // 라우터 설정
