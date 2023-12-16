0c956ffc5931a9704b6b0cae8b8891baee3ecbd9
ay0530 <edkdud530@gmail.com>
2023-12-16 16:20:15 +0900

    🔀 merge: feature/imageUrl(상권님) 브런치

--- src/routes/menus.router.js
+++ src/routes/menus.router.js
@@ -1,33 +1,44 @@
 // routes/menu.router.js
 
-import { Router } from "express"; // express 패키지
+import { Router } from 'express'; // express 패키지
 const menusRouter = Router();
-import { isLoggedIn } from "../middleware/verifyToken.middleware.js";
-import { validateEditMenu } from "../middleware/menuValidation.middleware.js";
-import MenusController from "../controller/menus.controller.js";
+import { isLoggedIn } from '../middleware/verifyToken.middleware.js';
+import { validateEditMenu } from '../middleware/menuValidation.middleware.js';
+import MenusController from '../controller/menus.controller.js';
+import upload from '../config/multerConfig.js';
 
 const menusController = new MenusController();
 
 // 메뉴 저장
-menusRouter.post("/menu", validateEditMenu, isLoggedIn, menusController.createMenu);
+menusRouter.post(
+  '/menu',
+  upload.single('image'),
+  validateEditMenu,
+  isLoggedIn,
+  menusController.createMenu,
+);
 // menusRouter.post("/menu", validateEditMenu, menusController.createMenu);
 
 // 메뉴 전체 조회
-menusRouter.get("/menu", menusController.getMenus);
+menusRouter.get('/menu', menusController.getMenus);
 
 // 메뉴 상세 조회
-menusRouter.get("/menu/:id", menusController.getMenu);
+menusRouter.get('/menu/:id', menusController.getMenu);
 
 // 메뉴 수정
-menusRouter.put("/menu/:id", validateEditMenu, isLoggedIn, menusController.updateMenu);
+menusRouter.put(
+  '/menu/:id',
+  validateEditMenu,
+  isLoggedIn,
+  menusController.updateMenu,
+);
 // menusRouter.put("/menu/:id", validateEditMenu, menusController.updateMenu);
 
 // 메뉴 삭제
-menusRouter.delete("/menu/:id", isLoggedIn, menusController.deleteMenu);
+menusRouter.delete('/menu/:id', isLoggedIn, menusController.deleteMenu);
 // menusRouter.delete("/menu/:id/:ownerId", menusController.deleteMenu);
 
 // 업종 전체 조회
-menusRouter.get("/food-category", menusController.getFoodCategory);
+menusRouter.get('/food-category', menusController.getFoodCategory);
 
 export default menusRouter;
-
