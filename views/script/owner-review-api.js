6756f94f0af228e2fc87e471835f418b7f94ad9f
ay0530 <edkdud530@gmail.com>
2023-12-16 17:42:24 +0900

    💄 style: 매장 리뷰 조회 페이지 프론트 작업

--- views/script/owner-review-api.js
+++ views/script/owner-review-api.js
@@ -12,17 +12,24 @@ document.addEventListener('DOMContentLoaded', function () {
     })
     .then((response) => {
       const reviews = response.data.data;
-      // console.log('reviews: ', reviews);
+      console.log('reviews: ', reviews);
       let count;
       // API 실행결과를 response로 받아와서 html 그려주기
       reviews.forEach((e, idx) => {
         count++;
         let temp_html = `
-        <div id="review_wrapper_${e.id}" class="review-wrapper">
-          <div class="hidden">${e.id}</div>
-          <div id="store_name">매장명</div>
-          <div class="form-control">${e.store.name}</div>
-          <div id="rating">별점</div>
+
+        <div class="p-4 border-b border-gray-200">
+          <div class="flex justify-between items-center px-2 py-0 sm:px-6"> 
+            <div>
+              <div class="font-semibold" style="font-size: 30px;">${e.customer.name}님</div>
+              <div class="text-xs text-gray-500">4시간 전(기능 추가 필요)</div>
+            </div>
+
+          </div>
+        </div>
+        <div id="review_wrapper_${e.id}" class="flex items-center px-4">
+          <div id="rating"></div>
           <div class="star_grade">
             <div class="review_comment_star_grade_wrapper${count}">
               <input type="radio" name="review_star_grade${count}" id="star1${count}" class="hidden_radio" value="1" />
@@ -37,10 +44,18 @@ document.addEventListener('DOMContentLoaded', function () {
               <label for="star5" class="custom_star"></label>
             </div>  
           </div>
-          <div id="menu_id">리뷰</div>
-          <div class="form-control">${e.comment}</div>
-          <div id="review_img">이미지</div>
-          <div class="form-control">이미지이이이이이</div>
+        </div>
+
+        <div class="flex items-center px-4 text-sm mt-2">
+          <p>${e.comment}</p>
+        </div>
+        <div class="mt-2 user-review-img mb-20">
+          <img
+            src="https://source.unsplash.com/featured/?food"
+            alt="Food"
+            class="rounded-lg"
+            style="height: 400px;"
+          />
         </div>
         `;
         $reviewContainer.insertAdjacentHTML('beforeend', temp_html);
@@ -68,24 +83,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-// 삭제 버튼을 눌렀을 때 실행하는 함수
-// 삭제 : 메뉴
-function clickDeleteBtn(clickedButton) {
-  event.stopPropagation(); // tr에 설정된 onclick 이벤트 실행 중단
-  const buttonId = clickedButton.id; // 클릭한 버튼의 ID 가져오기
-  const buttonIdArr = buttonId.split('_'); // 버튼 ID 쪼개기
-  const id = buttonIdArr[buttonIdArr.length - 1]; // 버튼 ID 쪼갠거에서 마지막 값인 id 값 가져오기
-
-  axios
-    .delete(`/api/reviews/${id}`, {
-      withCredentials: true,
-    })
-    .then((res) => {
-      // 삭제하기 전에 정말 삭제할거에요? 알림창으로 할까.. 근데 번거로운거같으니까 일단 클릭 시 바로 삭제되게 고!!!!
-      alert('삭제가 완료되었습니다.');
-      location.reload(); // 페이지 새로고침
-    })
-    .catch((error) => {
-      console.error('오류 발생:', error);
-    });
-}
