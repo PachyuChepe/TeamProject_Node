8df22516f8c39d834b4aff82488e925491bf84e0
PachyuChepe <pachyuchepe@gmail.com>
2023-12-17 13:03:35 +0900

    Feat: 고객 메뉴 주문 페이지 & 메뉴 생성 로직 완료

--- views/script/user-store-detail-etc.js
+++ views/script/user-store-detail-etc.js
@@ -1,54 +1,76 @@
 // 주문하기 버튼을 눌렀을 때 실행하는 함수
 // 저장 : 주문
 function clickCreateBtn(clickedButton) {
-  console.log('실행됨?');
-  event.stopPropagation(); // tr에 설정된 onclick 이벤트 실행 중단
-  const buttonId = clickedButton.id; // 클릭한 버튼의 ID 가져오기
-  const buttonIdArr = buttonId.split('_'); // 버튼 ID 쪼개기
-  const id = buttonIdArr[buttonIdArr.length - 1]; // 버튼 ID 쪼갠거에서 마지막 값인 id 값 가져오기
+  // 버튼 ID에서 메뉴 ID 추출
+  const buttonId = clickedButton.id;
+  const id = buttonId.split('_')[2];
 
-  const $price = document.getElementById(`price_${id}`).textContent;
-  const $quantity = document.getElementById(`quantity_input_${id}`).value;
-  // API로 전달할 값 JSON으로 설정
+  // 선택한 메뉴의 수량과 가격 가져오기
+  const quantityInput = document.getElementById(`quantity_input_${id}`);
+  const quantity = parseInt(quantityInput.value, 10);
+  const originalPrice = Number(
+    document.getElementById(`price_${id}`).getAttribute('data-original-price'),
+  );
+
+  // 총 가격 계산
+  const totalPrice = originalPrice * quantity;
+
+  // 주문 데이터 설정
   const data = {
     menuId: id,
     status: '배달전',
-    quantity: $quantity,
-    totalPrice: $price * $quantity,
+    quantity: quantity,
+    totalPrice: totalPrice,
   };
 
+  // 주문 데이터를 서버로 전송
   axios
     .post('/api/orders', data, {
       headers: { 'Content-Type': 'application/json; charset=UTF-8' },
       withCredentials: true,
     })
-    .then((res) => {
-      alert('주문이 완료되었습니다.');
-    })
-    .catch((error) => {
-      console.error('오류 발생:', error);
-    });
+    .then(() => alert('주문이 완료되었습니다.'))
+    .catch((error) => console.error('오류 발생:', error));
 }
 
+// 수량 증가 버튼 함수
 function clickIncleaseBtn(clickedButton) {
-  const buttonId = clickedButton.id; // 클릭한 버튼의 ID 가져오기
-  const buttonIdArr = buttonId.split('_'); // 버튼 ID 쪼개기
-  const id = buttonIdArr[buttonIdArr.length - 1]; // 버튼 ID 쪼갠거에서 마지막 값인 id 값 가져오기
+  // 버튼 ID에서 메뉴 ID 추출
+  const buttonId = clickedButton.id;
+  const id = buttonId.split('_')[2];
 
+  // 수량 증가 및 총 가격 업데이트
   const quantityInput = document.getElementById(`quantity_input_${id}`);
-  const currentValue = parseInt(quantityInput.value, 10);
-  quantityInput.value = currentValue + 1;
+  quantityInput.value = parseInt(quantityInput.value, 10) + 1;
+  updateTotalPrice(id);
 }
 
+// 수량 감소 버튼 함수
 function clickDecreaseBtn(clickedButton) {
-  const buttonId = clickedButton.id; // 클릭한 버튼의 ID 가져오기
-  const buttonIdArr = buttonId.split('_'); // 버튼 ID 쪼개기
-  const id = buttonIdArr[buttonIdArr.length - 1]; // 버튼 ID 쪼갠거에서 마지막 값인 id 값 가져오기
+  // 버튼 ID에서 메뉴 ID 추출
+  const buttonId = clickedButton.id;
+  const id = buttonId.split('_')[2];
 
+  // 수량 감소 및 총 가격 업데이트
   const quantityInput = document.getElementById(`quantity_input_${id}`);
   const currentValue = parseInt(quantityInput.value, 10);
-  if (currentValue > 0) {
-    // 수량이 0보다 클 때만 감소
+  if (currentValue > 1) {
     quantityInput.value = currentValue - 1;
+    updateTotalPrice(id);
   }
+}
+
+// 총 가격 업데이트 함수
+function updateTotalPrice(menuId) {
+  // 가격 엘리먼트 및 수량 입력값 가져오기
+  const priceElement = document.getElementById(`price_${menuId}`);
+  const quantityInput = document.getElementById(`quantity_input_${menuId}`);
+
+  // 총 가격 계산 및 표시
+  const originalPrice = Number(
+    priceElement.getAttribute('data-original-price'),
+  );
+  const quantity = parseInt(quantityInput.value, 10);
+  const totalPrice = (originalPrice * quantity).toLocaleString();
+  priceElement.textContent = `${totalPrice}원`;
 }
