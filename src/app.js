9915c5bcc85d38029763716b8e548fc3686bd828
PachyuChepe <pachyuchepe@gmail.com>
2023-12-17 15:56:52 +0900

    Fix: 클라이언트 기본 접속 경로 변경

--- src/app.js
+++ src/app.js
@@ -60,10 +60,10 @@ app.use('/api', [menusRouter]);
 app.use(errorHandler);
 
 // 프론트엔드 파일 서빙
-app.use(express.static('views'));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'user-login.html'));
+  res.sendFile(path.join(__dirname, '../views/user-login.html'));
 });
+app.use(express.static(path.join(__dirname, '../views')));
 
 // 데이터베이스 연결 확인 및 HTTPS/HTTP 서버 시작
 checkDatabaseConnection()
